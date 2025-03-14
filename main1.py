27a30a3af13362f4b5c144615d1913d643581b8e
bokus <bokus@tut.by>
2025-03-14 22:24:59 +0300

    add1

---
+++ main1.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+wd = os.getcwd()
+app = Flask(__name__, template_folder=wd)
+
+@app.route("/")
+def index():    
+    return render_template("1.html")
+
+
+@app.route("/sheet2/") 
+def sheet2():
+    return render_template("2.html")
+
+app.run()
