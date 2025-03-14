605ebd19dd5f9ee567f213365510fd63a23f5f6d
bokus <bokus@tut.by>
2025-03-15 01:53:37 +0300

    4

--- app1/init.sql
+++ app1/init.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS test_table (
     name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO test_table (name) VALUES ('Test Data 1'), ('Test Data 2');
+INSERT INTO test_table (name) VALUES ('Test Data 3'), ('Test Data 4');
